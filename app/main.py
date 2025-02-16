ab05e4141f25ce81dbc4ebce5fb1e20b8612de1c
Kolesnikov Artem <KolesnikovAD1998@yandex.ru>
2025-02-16 19:19:21 +0300

    lifespan cycle

--- app/main.py
+++ app/main.py
@@ -1,17 +1,23 @@
 # .env
 from dotenv import load_dotenv
+
 load_dotenv()
 
-
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.security import HTTPBearer
-
 from app.routers import auth, buy, info, sendCoin
 from app.database import engine, Base
 from fastapi.middleware.cors import CORSMiddleware
 
-
 security = HTTPBearer()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
 
 app = FastAPI(
     title="API Avito shop",
@@ -19,6 +25,7 @@ app = FastAPI(
     description="API для управления монетами, инвентарем и транзакциями.",
     docs_url="/docs",
     redoc_url="/redoc",
+    lifespan=lifespan,
 )
 
 app.add_middleware(
@@ -28,13 +35,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-@app.on_event("startup")
-async def startup():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
 
 app.include_router(auth.router)
 app.include_router(buy.router)
